ff1bc9e34696360d62ec5e204315b0bee6fc7fcb
roncofaber <fabrice.roncoroni@gmail.com>
2023-06-28 14:12:13 -0700

    updated spectra class

--- pymbxas/spectra.py
+++ pymbxas/spectra.py
@@ -6,10 +6,28 @@ Created on Mon Jun 26 10:33:37 2023
 @author: roncofaber
 """
 
+import numpy as np
+import os
+
+from .io.read import read_qchem_job
+
+#%%
 class XAS_spectra():
-    
-    def __init__(self, xdata, ydata):
-        
-        self.x = xdata
-        self.y = ydata
-        
+
+    def __init__(self,
+                 mbxas_spectra, #file with the mbxas spectra
+                 qchem_out = "qchem.output"
+                 ):
+
+        # define path of data
+        self.__path = os.path.dirname(mbxas_spectra)
+
+        # read spectra
+        self.x, self.y = np.loadtxt(mbxas_spectra).T
+
+
+        self.po = read_qchem_job(qchem_out, self.__path, "sp")
+
+        return
+
+    # def
--- pymbxas/utils/check_keywords.py
+++ pymbxas/utils/check_keywords.py
@@ -8,19 +8,29 @@ Created on Mon Jun 26 11:44:43 2023
 
 # Function to transform an occ. dict to input ready for QCHEM
 def determine_occupation(occupation):
-    
+
     if isinstance(occupation, str):
         return occupation
-    
+
     elif isinstance(occupation, dict):
-        
+
+        # check inject
+        if "inject" not in occupation:
+            occupation["inject"] = False
+
+        # generate occupation string
         occ_string = ""
         for channel in ["alpha", "beta"]:
-            
+
             indexes = [str(x) for x in range(1, occupation["nelectrons"]+1)
-                       if not (x == occupation["eject"] and channel == occupation["channel"])]
-            
+                       if not (x == occupation["eject"]
+                               and channel == occupation["channel"])]
+
+            if occupation["inject"] and channel == occupation["channel"]:
+                indexes.append(str(occupation["inject"]))
+
             occ_string += " ".join(indexes)
-            occ_string += "\n"
-    
+
+            occ_string += "\n  "
+
         return occ_string
