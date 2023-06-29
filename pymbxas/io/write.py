775ed53054df3e84d9459da17617ff458b7ca70f
roncofaber <fabrice.roncoroni@gmail.com>
2023-06-29 11:01:59 -0700

    input creation

--- pymbxas/io/write.py
+++ pymbxas/io/write.py
@@ -7,19 +7,15 @@ Created on Thu Jun 22 12:01:06 2023
 """
 
 import os
-
-from pyqchem import Structure, QchemInput
-from pyqchem.qc_input import CustomSection
-
-from pymbxas.utils.check_keywords import determine_occupation
+import pymbxas.build.input as inp
 
 #%%
 
 def write_mbxas_input(mbxas_parameters = {}, run_path = "."):
-        
+
     # define MBXAS input file
     mbxas_f = run_path + "/INP_FILE"
-    
+
     # copy default params and update with input ones
     mbxas_params = {
         "gridP"    : 100,
@@ -40,9 +36,9 @@ def write_mbxas_input(mbxas_parameters = {}, run_path = "."):
         "output_file"   : "{}/qchem.output".format(run_path),
         "printanalysis" : True
         }
-    
+
     mbxas_params.update(mbxas_parameters)
-        
+
     # write input file
     with open(mbxas_f, "w") as fout:
         for key, value in mbxas_params.items():
@@ -54,51 +50,30 @@ def write_mbxas_input(mbxas_parameters = {}, run_path = "."):
 def write_qchem_job(molecule, charge, multiplicity,
                     qchem_params, run_path, occupation = None,
                     from_scratch = True):
-    
+
     # make dir if not existent
     if not os.path.isdir(run_path):
         os.mkdir(run_path)
-    
+
     # if from scratch write new file, otherwise append
     if from_scratch:
         write_mode = "w"
     else:
         write_mode = "a"
-    
-    # make molecule
-    molecule_str = Structure(
-        coordinates  = molecule.get_positions(),
-        symbols      = molecule.get_chemical_symbols(),
-        charge       = charge,
-        multiplicity = multiplicity)
-    
-    # check occupation if needed
-    
-    if occupation is not None:
-        
-        # check occupation format
-        occupation = determine_occupation(occupation)
-        
-        occ_section = CustomSection(title='occupied',
-                                    keywords={' ' : occupation})
-        
-        if isinstance(qchem_params["extra_sections"], list):
-            qchem_params["extra_sections"].append(occ_section)
-        else:
-            qchem_params["extra_sections"] = occ_section
-            
-    # generate input
-    molecule_str_input = QchemInput(
-            molecule_str,
-            **qchem_params,
-            )
-    
+
+    # generate qchem input
+    qchem_input = inp.make_qchem_input(molecule, charge,
+                                       multiplicity, qchem_params,
+                                       occupation=occupation)
+
+
+
     # write input in target path (append mode)
     with open(run_path + "qchem.input", write_mode) as fout:
-        
+
         if write_mode == "a":
             fout.write("\n@@@\n\n")
-        
-        fout.write(molecule_str_input.get_txt())
-        
+
+        fout.write(qchem_input.get_txt())
+
     return
