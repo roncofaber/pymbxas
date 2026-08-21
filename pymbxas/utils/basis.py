94cb775f80f1dd98f8aded8b8262bfeed11f5798
roncofaber <fabrice.roncoroni@gmail.com>
2026-08-21 15:15:49 -0700

    Remove unused modules and helpers

--- pymbxas/utils/basis.py
+++ pymbxas/utils/basis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Functions to read basis set and get info
+Functions to work with basis sets
 
 Created on Thu Jul 13 15:57:59 2023
 
@@ -11,52 +11,6 @@ Created on Thu Jul 13 15:57:59 2023
 import numpy as np
 
 #%%
-
-# function that finds the permutation needed to reorder shells
-# to fchk file format (IQmol compatible)
-def calculate_shell_reordering(shell, start_idx):
-
-    index_list = []
-
-    if shell["shell_type"].startswith("d"):
-        index_list = [ii + start_idx for ii in [4,2,0,1,3]]
-    elif shell["shell_type"].startswith("f"):
-        index_list = [ii + start_idx for ii in [6,4,2,0,1,3,5]]
-    elif any([shell["shell_type"].startswith(ss) for ss in ["s", "p"]]):
-        index_list = [ii + start_idx for ii in range(shell["functions"])]
-    else:
-        raise Exception("Sorry, shell type {} not recognized".format(
-            shell["shell_type"]))
-
-    return index_list
-
-
-# get info from basis set
-def get_basis_set_info(basis):
-    atom_coeffs = []
-    atom_labels = []
-    symbols     = []
-    nbasis = 0
-    indexing    = []
-    for cc, atom in enumerate(basis['atoms']):
-        istart = nbasis
-        atom_label = []
-        for shell in atom['shells']:
-
-            # store internal ordering
-            indexing.extend(calculate_shell_reordering(shell, nbasis))
-
-            # update number of basis
-            nbasis += shell['functions']
-
-            # store shell types
-            atom_label.extend(shell['functions']*[shell['shell_type']])
-
-        atom_coeffs.append(np.array(range(istart, nbasis)))
-        atom_labels.append(np.array(atom_label))
-        symbols.append(atom["symbol"])
-
-    return atom_coeffs, atom_labels, symbols, nbasis, indexing
 
 # calculate AO permutation from structure permutation
 def get_AO_permutation(mol, permutation):
--- pymbxas/utils/check_keywords.py
+++ pymbxas/utils/check_keywords.py
@@ -11,42 +11,6 @@ Created on Mon Jun 26 11:44:43 2023
 
 #%%
 
-# default excitation dict
-def_exc_dict = {
-    "channel" : "beta",
-    "eject"   : False,
-    "inject"  : False,
-    }
-
-# Function to transform an occ. dict to an occupation string
-def determine_occupation(occupation):
-
-    if isinstance(occupation, str):
-        return occupation
-
-    elif isinstance(occupation, dict):
-
-        # check inject
-        if "inject" not in occupation:
-            occupation["inject"] = False
-
-        # generate occupation string
-        occ_string = ""
-        for cc, channel in enumerate(["alpha", "beta"]):
-
-            indexes = [str(x) for x in range(1, occupation["nelectrons"][cc]+1)
-                       if not (x == occupation["eject"]
-                               and channel == occupation["channel"])]
-
-            if occupation["inject"] and channel == occupation["channel"]:
-                indexes.append(str(occupation["inject"]))
-
-            occ_string += " ".join(indexes)
-
-            occ_string += "\n  "
-
-        return occ_string
-    
 #TODO expand this to work with mixed PBCs, works only for full pbc at the moment
 def check_pbc(pbc, structure):   
     if pbc is None:
